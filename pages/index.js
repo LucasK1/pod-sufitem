8e48224364507241a3f67d7a70d7f02e2d480ff2
LucasK1 <l.kozlowski@mailbox.org>
2021-01-11 17:41:59 +0100

    Create footer, style it roughly

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,100 @@
 export default function Home() {
   return (
     <>
-      <article className="main__text">
-        <h1>Hohooo ale szkoła</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
-          deserunt maxime quis consectetur minus architecto! Officia voluptatum,
-          autem, fugit, beatae dolorem repellat commodi repudiandae libero culpa
-          nisi quasi nam amet magnam? Mollitia beatae voluptates, porro impedit
-          ullam ipsum culpa ad illo labore cupiditate tempore incidunt,
-          repellendus sapiente, laudantium nisi inventore consequuntur ducimus
-          perferendis! Mollitia aliquid velit, a dolorum deserunt impedit
-          suscipit nisi praesentium sequi. Qui aut pariatur cumque accusamus
-          consequatur ratione ullam aliquid, doloribus soluta inventore rem
-          vero, tempora saepe asperiores odio adipisci nisi. Beatae sunt
-          cupiditate ipsa! Necessitatibus dicta consequuntur nam soluta
-          voluptatum pariatur laboriosam. Facilis autem eum recusandae.
-        </p>
-      </article>
-      <a href="#">
-        <div className="main__booking-btn">
-          <p>Zapisy na zajęcia</p>
-          <p>-kliknij TU-</p>
-        </div>
-      </a>
+      <div className="main">
+        <main>
+          <article className="main__text">
+            <h1>Hohooo ale szkoła</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Voluptatum deserunt maxime quis consectetur minus architecto!
+              Officia voluptatum, autem, fugit, beatae dolorem repellat commodi
+              repudiandae libero culpa nisi quasi nam amet magnam? Mollitia
+              beatae voluptates, porro impedit ullam ipsum culpa ad illo labore
+              cupiditate tempore incidunt, repellendus sapiente, laudantium nisi
+              inventore consequuntur ducimus perferendis! Mollitia aliquid
+              velit, a dolorum deserunt impedit suscipit nisi praesentium sequi.
+              Qui aut pariatur cumque accusamus consequatur ratione ullam
+              aliquid, doloribus soluta inventore rem vero, tempora saepe
+              asperiores odio adipisci nisi. Beatae sunt cupiditate ipsa!
+              Necessitatibus dicta consequuntur nam soluta voluptatum pariatur
+              laboriosam. Facilis autem eum recusandae.
+            </p>
+          </article>
+          <a href="#">
+            <div className="main__booking-btn">
+              <p>Zapisy na zajęcia</p>
+              <p>-kliknij TU-</p>
+            </div>
+          </a>
+        </main>
+        <footer className="main__footer">
+          <div className="main__footer-item main__footer-contact">
+            <span className="main__footer-item--title">
+              Skontaktuj się z nami
+            </span>
+            <div className="main__footer-item--content"></div>
+          </div>
+          <div className="main__footer-item main__footer-social">
+            <span className="main__footer-item--title">Znajdź nas w sieci</span>
+            <div className="main__footer-item--content"></div>
+          </div>
+          <div className="main__footer-item main__footer-address">
+            <span className="main__footer-item--title">Odwiedź nas</span>
+            <div className="main__footer-item--content"></div>
+          </div>
+        </footer>
+      </div>
       <style jsx>{`
-        .main__text {
-          text-align: center;
+        main {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
         }
-        .main__booking-btn {
-          align-text: center;
-          width: 200px;
-          color: #fff;
-          font-size: 18px;
-          padding: 20px;
-          border-radius: 20px;
-          background-color: #000;
-          & > p {
-            line-height: 15px;
+
+        .main {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          &__text {
+            text-align: center;
+          }
+          &__booking-btn {
+            text-align: center;
+            width: 200px;
+            color: #fff;
+            font-size: 18px;
+            padding: 20px;
+            border-radius: 20px;
+            background-color: #000;
+            & > p {
+              line-height: 15px;
+            }
+          }
+        }
+
+        .main__footer {
+          width: 100%;
+          max-width: 1200px;
+          display: grid;
+          grid-template-columns: repeat(3, 1fr);
+          position: fixed;
+          bottom: 0;
+
+          &-social {
+            & > .main__footer-item--content {
+              border-right: 1px solid #000;
+              border-left: 1px solid #000;
+            }
+          }
+
+          &-item--title {
+            font-size: 18px;
+            line-height: 28px;
+            font-weight: 500;
+          }
+          &-item--content {
+            height: 100px;
           }
         }
       `}</style>
