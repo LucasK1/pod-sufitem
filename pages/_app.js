0a0c89732e931f547d2cd998138b5cc3506cc35d
LucasK1 <l.kozlowski@mailbox.org>
2021-01-14 13:25:50 +0100

    Add icons, style them, make them clickable, adjust spacing. Unify global styling
    Global styles were in two files, I moved everything to one file.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import Navbar from '../components/Navbar/Navbar';
-import '../styles/globals.scss';
 import '../styles/styles.scss';
 import '../styles/normalize.css';
-import { Head } from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
