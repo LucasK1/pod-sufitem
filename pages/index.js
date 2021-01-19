6431a14fc9bc874e3776fa48029b61264c35cb84
LucasK1 <l.kozlowski@mailbox.org>
2021-01-19 22:12:27 +0100

    Remove unnecessary import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
 import Prices from '../components/Cennik/Prices';
 import InfoBox from '../components/InfoBox/InfoBox';
+
 import styles from '../styles/landingPage.module.scss';
 
 const Home = () => {
@@ -27,7 +29,8 @@ const Home = () => {
           activeClassDesc === item ? styles.classes__selectionBtn_isActive : ''
         }`}
         onClick={(e) => classClickHandler(e, item)}
-        key={item}>
+        key={item}
+      >
         {item}
       </a>
     );
@@ -111,7 +114,8 @@ const Home = () => {
             </div>
 
             <div
-              className={`${styles.contact__column} ${styles.contact__social}`}>
+              className={`${styles.contact__column} ${styles.contact__social}`}
+            >
               <span className={styles.contact__column_title}>
                 Znajdź nas w sieci
               </span>
@@ -121,14 +125,16 @@ const Home = () => {
                 </a>
                 <a
                   href="https://www.instagram.com/pod.sufitem/"
-                  target="_blank">
+                  target="_blank"
+                >
                   <i className="fab fa-instagram-square"></i>
                 </a>
               </div>
             </div>
 
             <div
-              className={`${styles.contact__column} ${styles.contact__address}`}>
+              className={`${styles.contact__column} ${styles.contact__address}`}
+            >
               <span className={styles.contact__column_title}>Odwiedź nas</span>
               <div className={styles.contact__column_content}>
                 <a href="https://goo.gl/maps/vRQJRCQE7JP4J6CWA" target="_blank">
