a398650f3c0f5fd1e5d9d6016886e7f3f6b64532
LucasK1 <l.kozlowski@mailbox.org>
2021-01-26 13:00:48 +0100

    Create a JSON file for the teachers and add a few entries

--- components/Teachers/Teachers.js
+++ components/Teachers/Teachers.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Modal from '../UI/Modal/Modal';
 
 import styles from './Teachers.module.scss';
+import teachers from './teachers.json';
 
 const Teachers = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -17,9 +18,9 @@ const Teachers = () => {
     setSelectedTeacher('');
     setModalOpen(false);
   }
-
+/*
   const teachers = {
-    Alina: 'Baaardzo dlugi opis',
+    'Anna Łukasiak': 'Założycielka studia Pod Sufitem, instruktorka pole dance, aerial hoop i stretchingu, absolwentka Akademii Wychowania Fizycznego w Warszawie na kierunkach: fizjoterapia i wychowanie fizyczne.</br>Jako nastolatka tańczyła, grała w siatkówkę i biegała. Na pole dance trafiła dość późno, ale lepiej późno niż wcale ;) W gimnastyce powietrznej najbardziej ceni jej niekończące się możliwości rozwoju. Uwielbia figury gibkościowe, ale nieustannie pracuje nad siłą, dynamiką i nowymi kombinacjami, aby stać się wszechstronnym sportowcem. Jej zajęcia charakteryzują się porządną rozgrzewką, profesjonalnym podejściem i swobodną atmosferą. Jako instruktor dba o to, aby regularnie zdobywać nową wiedzę i uczestniczyć w szkoleniach.</br></br>Osiągnięcia:<ul><li>I miejsce Sensual Championships 2018</li><li>III miejsce II Akademickie zawody pole dance 2018</li><li>III miejsce Vertical fit Championschip 2018</li><li>II miejsce Duende International Contest 2017</li></ul>Ponadto brała udział w wielu treningach, warsztatach, szkoleniach, kursach, sesjach zdjęciowych i pokazach cały czas poszerzając swoją wiedzę oraz propagując piękny sport, którym jest pole dance.',
     Balladyna: '',
     Kalafonia: '',
     Scholastyka: '',
@@ -30,7 +31,7 @@ const Teachers = () => {
     Antananarywa: '',
     Endywia: '',
   };
-
+*/
   return (
     <section className={styles.container}>
       {Object.keys(teachers).map((teacher) => (
@@ -47,8 +48,8 @@ const Teachers = () => {
         </div>
       ))}
       <Modal show={modalOpen} closeModal={closeModal}>
-        <h1>{selectedTeacher}</h1>
-        <p>{teachers[selectedTeacher]}</p>
+        <h4 className={styles.modal__title}>{selectedTeacher}</h4>
+        <p className={styles.modal__desc} dangerouslySetInnerHTML={{__html: teachers[selectedTeacher]}}></p>
       </Modal>
     </section>
   );
