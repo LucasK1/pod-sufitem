a6ac9d43545c929b7f2ae3b875ff8038b3d08af3
LucasK1 <l.kozlowski@mailbox.org>
2021-02-15 17:38:03 +0100

    Remove tooltips in mobile view

--- components/Prices/PriceItem/PriceItem.js
+++ components/Prices/PriceItem/PriceItem.js
@@ -1,44 +1,58 @@
 import Tippy from '@tippyjs/react/headless';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './PriceItem.module.scss';
 
 const PriceItem = ({ name, price, desc, tippyPlacement }) => {
+  const [windowWidth, setWindowWidth] = useState(1800);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
   const splitDesc = desc
     .split('. ')
     .map((item, index, array) =>
       index < array.length - 1 ? `${item}.` : item
     );
 
+  const priceLink = (
+    <a
+      href="https://app.fitssey.com/podsufitem/frontoffice/pricing/cards"
+      target="_blank"
+      tabIndex="0"
+      className={styles.priceItem__name}>
+      {name}
+    </a>
+  );
+
   return (
     <>
       <li className={styles.priceItem}>
-        <Tippy
-          render={(attrs) => (
-            <div
-              className={`${styles.tooltipBox} ${
-                tippyPlacement === 'right'
-                  ? styles.tooltipArrowLeft
-                  : styles.tooltipArrowRight
-              }`}
-              {...attrs}>
-              <ul className={styles.descList}>
-                {splitDesc.map((item) => (
-                  <li>{item}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-          placement={tippyPlacement}
-          delay={[50, 150]}>
-          <a
-            href="https://app.fitssey.com/podsufitem/frontoffice/pricing/cards"
-            target="_blank"
-            tabIndex="0"
-            className={styles.priceItem__name}>
-            {name}
-          </a>
-        </Tippy>
+        {windowWidth >= 600 ? (
+          <Tippy
+            render={(attrs) => (
+              <div
+                className={`${styles.tooltipBox} ${
+                  tippyPlacement === 'right'
+                    ? styles.tooltipArrowLeft
+                    : styles.tooltipArrowRight
+                }`}
+                {...attrs}>
+                <ul className={styles.descList}>
+                  {splitDesc.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            placement={tippyPlacement}
+            delay={[50, 150]}>
+            {priceLink}
+          </Tippy>
+        ) : (
+          priceLink
+        )}
         <span>{price}zł</span>
       </li>
     </>
