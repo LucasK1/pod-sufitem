f2ab51d0de2d4a63f9ce53a37b321ad6581598c2
LucasK12 <l.kozlowski@mailbox.org>
2021-08-17 21:10:56 +0200

    Add new prices, remove old teachers

--- components/prices/prices.js
+++ components/prices/prices.js
@@ -6,16 +6,15 @@ const Prices = () => {
   return (
     <section className={styles.content}>
       <div className={styles.content__section}>
-        <h4 className={styles.content__sectionTitle}>Pole Dance/Aerial Hoop</h4>
-        <h5>(Obowiązuje na zajęcia pole dance, aerial)</h5>
+        <h4 className={styles.content__sectionTitle}>Wejścia jednorazowe</h4>
         <ul className={styles.content__priceList}>
-          {prices.poleAerial.map(({ id, name, price }) => (
+          {prices.single.map(({ id, name, price }) => (
             <PriceItem key={id} name={name} price={price} />
           ))}
         </ul>
       </div>
       <div className={styles.content__section}>
-        <h4 className={styles.content__sectionTitle}>Fitness</h4>
+        <h4 className={styles.content__sectionTitle}>Karnety fitness</h4>
         <h5>(Obowiązuje na zajęcia z wyłączeniem pole dance i aerial hoop)</h5>
         <ul className={styles.content__priceList}>
           {prices.fitness.map(({ id, name, price }) => (
@@ -24,7 +23,7 @@ const Prices = () => {
         </ul>
       </div>
       <div className={styles.content__section}>
-        <h4 className={styles.content__sectionTitle}>Mix</h4>
+        <h4 className={styles.content__sectionTitle}>Karnety Mix</h4>
         <h5>(Obowiązuje na wszystkie zajęcia)</h5>
         <ul className={styles.content__priceList}>
           {prices.mix.map(({ id, name, price }) => (
@@ -34,7 +33,6 @@ const Prices = () => {
       </div>
       <div className={styles.content__section}>
         <h4 className={styles.content__sectionTitle}>Wynajem sali</h4>
-        <h5>(dostępne 3 sale)</h5>
         <ul className={styles.content__priceList}>
           {prices.rent.map(({ id, name, price }) => (
             <PriceItem key={id} name={name} price={price} />
