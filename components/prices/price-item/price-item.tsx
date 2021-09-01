5fb6ee83169ba5b52378a0c52f123f635571be9c
LucasK12 <l.kozlowski@mailbox.org>
2021-09-01 21:10:34 +0200

    Start conversion to tsc

---
+++ components/prices/price-item/price-item.tsx
@@ -0,0 +1,19 @@
+import styles from './price-item.module.scss';
+
+const PriceItem = ({ name, price }) => {
+  return (
+    <li className={styles.priceItem}>
+      <a
+        href="https://app.fitssey.com/podsufitem/frontoffice/pricing/cards"
+        target="_blank"
+        rel="noreferrer"
+        tabIndex={0}
+        className={styles.priceItem__name}
+      >
+        {name}
+        <span> - {price}zł</span>
+      </a>
+    </li>
+  );
+};
+export default PriceItem;
